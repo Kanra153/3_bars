2588164870f41a6ee5d50f8db376ef9afbd136bf
Kanra153 <mad_hatter153@bk.ru>
2018-02-14 09:25:23 +0300

    first commit

--- bars.py
+++ bars.py
@@ -1,12 +1,27 @@
 import json
+import os
+import sys
+from math import sqrt
+from math import pow
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file_handler:
+        parsed_json = json.load(file_handler)
+    return parsed_json
 
 
 def get_biggest_bar(data):
-    pass
+    bars = parsed_json['features']
+    seats = []
+    for bar in bars:
+        seats.append(bar['properties']['Attributes']['SeatsCount'])
+    biggest_seats = max(seats)
+    for bar in bars:
+        if bar['properties']['Attributes']['SeatsCount'] == biggest_seats: #TODO: Сделать с помощью min()/max()
+            biggest_bar = bar
+    return(biggest_bar['properties']['Attributes']['Name'])
+
 
 
 def get_smallest_bar(data):
@@ -18,4 +33,7 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    file_path = sys.argv[1]
+    if os.path.exists(file_path):
+        parsed_json = load_data(file_path)
+        get_biggest_bar(parsed_json)
